ba26c3ccd66e686ee1bd7c98f245fd9440596e31
Nelly <nasyrova.nelli@mail.ru>
2022-01-07 13:10:14 +0300

    регистрация

--- actions/login.php
+++ actions/login.php
@@ -2,21 +2,21 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
 $login = $_POST['login'];
-$PASSWORD = $_POST['password'];
+$PASSWORD = md5($_POST['password']);
 
 $query = 'SELECT * FROM users WHERE login = :login AND password = :password';
-$res = $pdo->prepare($query);
+$RES = $pdo->prepare($query);
 $res->execute([
     ':login' => $login,
     ':password' => $password
 ]);
 
-$user = $res->fetchAll();
+$user = $res->fetch();
 
 if ($user) {
     $_SESSION['user'] = $user;
-    HEADER('Location:../index.php');
+    header('Location:../index.php');
 } else{
-    $_SESSION['login_error'] = true;
+    $_SESSION['loginError'] = true;
     header('Location: ../pages/login.php');
 }
